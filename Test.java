7b8cc81f76266ecc6d5f6b88448d7c2779e4bb4e
shimon1351992 <73971771+shimon1351992@users.noreply.github.com>
2022-07-19 14:32:30 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -9,24 +9,24 @@ public class Test {
 
 
     public static void main(String args[]) throws IOException {
-        ServerSocket ss = new ServerSocket(6666);
-        Socket s = ss.accept();
-        DataInputStream dis = new DataInputStream(s.getInputStream());
-        String str = (String) dis.readUTF();
+//         ServerSocket ss = new ServerSocket(6666);
+//         Socket s = ss.accept();
+//         DataInputStream dis = new DataInputStream(s.getInputStream());
+//         String str = (String) dis.readUTF();
 
-        File folder = new File(str);
-        File[] listOfFiles = folder.listFiles();
+//         File folder = new File(str);
+//         File[] listOfFiles = folder.listFiles();
 
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile()) {
-                System.out.println("File " + listOfFiles[i].getName());
-            } else if (listOfFiles[i].isDirectory()) {
-                System.out.println("Directory " + listOfFiles[i].getName());
-            }
-        }
-        System.out.print("Client Says = " +str);
-        ss.close();
-        }
+//         for (int i = 0; i < listOfFiles.length; i++) {
+//             if (listOfFiles[i].isFile()) {
+//                 System.out.println("File " + listOfFiles[i].getName());
+//             } else if (listOfFiles[i].isDirectory()) {
+//                 System.out.println("Directory " + listOfFiles[i].getName());
+//             }
+//         }
+        System.out.print("Client Says");
+//         ss.close();
+//         }
 
     }
 
