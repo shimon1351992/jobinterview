2fa9fe4bd7c5ec9487b14567c563c5982128d075
shimon1351992 <73971771+shimon1351992@users.noreply.github.com>
2022-07-19 13:54:59 +0300

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,16 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+
+
+    public static void main(String[] args) throws IOException {
+        Socket s = new Socket("192.168.56.1",6666);
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        dout.writeUTF("C:\\WINDOWS");
+        dout.flush();
+        dout.close();
+        s.close();
+    }
+}
---
+++ Test.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+public class Test {
+
+
+
+    public static void main(String args[]) throws IOException {
+
+
+        ServerSocket ss = new ServerSocket(6666);
+        Socket s = ss.accept();
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        String str = (String) dis.readUTF();
+
+        File folder = new File(str);
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+        System.out.print("Client Says = " +str);
+        ss.close();
+        }
+
+    }
+
+
